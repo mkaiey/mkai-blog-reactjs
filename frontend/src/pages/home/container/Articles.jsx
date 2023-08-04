6804981c8ba46cdafdfba501a51818c809aa9859
Mkai Ey <139222639+mkaiey@users.noreply.github.com>
2023-08-05 04:45:53 +0700

    first commit

---
+++ frontend/src/pages/home/container/Articles.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { FaArrowRight } from "react-icons/fa";
+
+import ArticleCard from "../../../components/ArticleCard";
+import { useQuery } from "@tanstack/react-query";
+import { getAllPosts } from "../../../services/index/posts";
+import { toast } from "react-hot-toast";
+import ArticleCardSkeleton from "../../../components/ArticleCardSkeleton";
+import ErrorMessage from "../../../components/ErrorMessage";
+
+const Articles = () => {
+  const { data, isLoading, isError } = useQuery({
+    queryFn: () => getAllPosts(),
+    queryKey: ["posts"],
+    onError: (error) => {
+      toast.error(error.message);
+      console.log(error);
+    },
+  });
+
+  return (
+    <section className="container mx-auto flex flex-col px-5 py-10">
+      <div className=" flex flex-wrap gap-y-5 pb-10 md:gap-x-5">
+        {isLoading ? (
+          [...Array(3)].map((item, index) => (
+            <ArticleCardSkeleton
+              key={index}
+              className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]"
+            />
+          ))
+        ) : isError ? (
+          <ErrorMessage message="Couldn't fetch the posts data" />
+        ) : (
+          data?.data.map((post) => (
+            <ArticleCard
+              key={post._id}
+              post={post}
+              className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]"
+            />
+          ))
+        )}
+      </div>
+      <button className="mx-auto flex items-center gap-x-2 rounded-lg border-2 border-primary px-6 py-3 font-bold text-primary">
+        <span>More articles</span>
+        <FaArrowRight className="h-3 w-3" />
+      </button>
+    </section>
+  );
+};
+
+export default Articles;
---
+++ frontend/src/pages/home/container/Hero.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { FiSearch } from "react-icons/fi";
+
+import { images } from "../../../constants";
+
+const Hero = () => {
+  return (
+    <section className="container mx-auto flex flex-col px-5 py-5 lg:flex-row">
+      <div className="mt-10 lg:w-1/2">
+        <h1 className="text-center font-roboto text-3xl font-bold text-dark-soft md:text-5xl lg:max-w-[540px] lg:text-left lg:text-4xl xl:text-5xl">
+          Read the most interesting articles
+        </h1>
+        <p className="mt-4 text-center text-dark-light md:text-xl lg:text-left lg:text-base xl:text-xl">
+          Generating catchy titles requires creating messages that excite and
+          resonate with your audience, hinting at the main objective of your
+          post, and conveying that you are a trusted source of information.
+          Combine these ingredients, and you will be sure to have a title that
+          excites, engages, and informs.
+        </p>
+        <div className="relative mt-10 flex flex-col gap-y-2.5 lg:mt-6 xl:mt-10">
+          <div className="relative">
+            <FiSearch className="absolute left-3 top-1/2 h-6 w-6 -translate-y-1/2 text-[#959EAD]" />
+            <input
+              className="w-full rounded-lg py-3 pl-12 pr-3 font-semibold text-dark-soft shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] placeholder:font-bold placeholder:text-[#959EAD] focus:outline-none md:py-4"
+              type="text"
+              placeholder="Search article"
+            />
+          </div>
+          <button className="w-full rounded-lg bg-primary px-5 py-3 font-semibold text-white md:absolute md:right-2 md:top-1/2 md:w-fit md:-translate-y-1/2 md:py-2">
+            Search
+          </button>
+        </div>
+        <div className="mt-4 flex flex-col lg:mt-7 lg:flex-row lg:flex-nowrap lg:items-start lg:gap-x-4">
+          <span className="mt-2 font-semibold italic text-dark-light lg:mt-4 lg:text-sm xl:text-base">
+            Popular Tags:
+          </span>
+          <ul className="mt-3 flex flex-wrap gap-x-2.5 gap-y-2.5 lg:text-sm xl:text-base">
+            <li className="rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 font-semibold text-primary">
+              Design
+            </li>
+            <li className="rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 font-semibold text-primary">
+              User Experience
+            </li>
+            <li className="rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 font-semibold text-primary">
+              User Interfaces
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="lg:1/2 hidden lg:block">
+        <img
+          className="px-5 py-5"
+          src={images.HeroImage}
+          style={{ height: 400, width: 600 }}
+          alt="wellcome to mkai-blog"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
